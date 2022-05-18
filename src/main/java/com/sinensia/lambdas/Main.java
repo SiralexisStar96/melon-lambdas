01e32b8f2778cbfc9913b5c132201b193a5abeee
Alejandro Garcia Villar <alejandro.garcia-villar@gft.com>
2022-05-18 10:59:02 +0200

    He creado la clase melon, con su constructor, getter y toString().
    Y también he agregado imports a la clase Main.
    
    He añadido una funcion de filtrado

--- src/main/java/com/sinensia/lambdas/Main.java
+++ src/main/java/com/sinensia/lambdas/Main.java
@@ -1,5 +1,6 @@
 package com.sinensia.lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,11 +15,45 @@ public class Main {
                 new Melon("Bailan", 1300, "China")
         );
 
-        for (int i = 0; i < melons.size(); ++i) {
-            System.out.println(melons.get(i).toString());
+        System.out.println("Esta es la lista melones:");
+       for (int i = 0; i < melons.size(); ++i) {
+          System.out.println(melons.get(i).toString());
         }
 
+        System.out.println("");
 
+        //Generamos un lista de Objetos Melones y filtramos por tipo.
+        List<Melon> bailan = filterByType(melons, "Bailan");
+
+        //Imprimimos el resultado.
+        System.out.println("Esta es la lista melones bailan:");
+        for (int i = 0; i < bailan.size(); ++i) {
+
+            System.out.println(bailan.get(i).toString());
+        }
+
+    }
+
+
+    //Creamos un metodo statico para filtrar los tipos de melones
+    private static List<Melon> filterByType(List<Melon> melons, String bailan) {
+        //Primero guardamos un sitio donde guardar los melones
+        List<Melon> x = new ArrayList<>();
+
+        //Iteramos dentro de la propia lista de melones
+        for(int i=0 ; i< melons.size(); ++i) {
+
+            //Buscamos entre nuestra lista los de tipo Bailan
+            //Hacemos control de nulos
+
+            if (melons!= null && melons.get(i).getType().equals("Bailan")) {
+
+                //Añadimos a la nueva lista
+                x.add(melons.get(i));
+
+            }
+        }
+        return x;
 
     }
 }
